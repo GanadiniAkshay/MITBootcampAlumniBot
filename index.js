605e1d841e117357a35b8704c1b957b686e0f05a
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-09 20:37:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+//==============================================
+// Bot Setup
+//==============================================
+
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || process.env.port || 3978, function(){
+    console.log('%s is listening to %s',server.name, server.url);
+});
+
+//Create Chat Bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
