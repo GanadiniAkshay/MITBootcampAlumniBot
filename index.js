1daecc81bc49aaac443c34504cff1aac95c2877f
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-20 00:42:20 +0530

    added whatIsBootcamp

--- index.js
+++ index.js
@@ -102,8 +102,11 @@ intents.matches('hello',[
 intents.matches('whatIsBootcamp',[
     function (session){
         session.sendTyping();
-        text = "The MIT Global Entrepreneurship Bootcamp is an intensive, week-long new ventures leadership program that gives bootcampers a taste of drinking from the firehose that all MIT students experience. It is a nexus connecting entrepreneurs from around the world to the entrepreneurship and         innovation eco-system surrounding the Massachusetts Institute of Technology";
+        text = "The MIT Global Entrepreneurship Bootcamp is an intensive, week-long new ventures leadership program that gives bootcampers a taste of drinking from the firehose that all MIT students experience."
+        text1 ="It is a nexus connecting entrepreneurs from around the world to the entrepreneurship and innovation eco-system surrounding the Massachusetts Institute of Technology";
         session.send(text);
+        session.sendTyping();
+        session.send(text1);
     }
 ]);
 
