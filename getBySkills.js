763947729ffdf64a4422029761e58491c4a28be7
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-25 13:53:43 +0530

    testing splitting into multiple files

--- getBySkills.js
+++ getBySkills.js
@@ -1,4 +1,13 @@
+var builder = require('botbuilder');
 module.exports = {
+
+    //Create Chat Bot
+    var connector = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
+
+    var bot = new builder.UniversalBot(connector);
     bot.dialog('/searchByLanguage',[
         function (session,args,next){
             session.send("Searching by Language spoken");
