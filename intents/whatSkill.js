7aaa23a2fe8173b86c097c8005d47eb93484dc04
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 15:52:01 +0530

    added whatSkill

--- intents/whatSkill.js
+++ intents/whatSkill.js
@@ -4,50 +4,7 @@ module.exports = function (intents,builder){
             if (session.userData.isBootcamper != 'true'){
                 session.endDialog('You can ask general questions about the bootcamp');
             }else{
-                var User = require('../models/user');
-                var skill = builder.EntityRecognizer.findEntity(args.entities,'skill');
-                if (skill){
-                    var  profession_map = {
-                        "programmer" : 'programming',
-                        "programs"   : 'programming',
-                        "developer"  : 'programming',
-                        "develops"   : 'programming',
-                        "code"       : 'programming',
-                        "coding"     : 'programming',
-                        "coder"      : 'programming',
-                        "codes"      : 'programming',
-                        "paints"     : 'painting',
-                        "painter"    : 'painting',
-                        "photographer" : 'photography',
-                        "videographer" : 'videography',
-                        "seller"       : 'selling',
-                        "teacher"      : 'teaching'
-                    };
-                    if (skill in profession_map){
-                        skill = profession_map[skill];
-                    }
-                    User.find({"skills":{$in:[skill]}},function(err,campers){
-                        if (campers.length > 0){
-                            found = 1;
-                            attachments = [];
-                            for (j=0;j<campers.length;j++){
-                                attachments.push(
-                                    new builder.HeroCard(session)
-                                        .title(campers[j]['name'])
-                                        .subtitle(campers[j]["cohort"] + " Lives in " + campers[j]['residence_country'] + '. email : ' + campers[j]['email'])
-                                )
-                            }
-                            var msg = new builder.Message(session).attachments(attachments);
-                            session.endDialog(msg);
-                        }
-
-                        else{
-                                session.endDialog("Couldn't find any bootcampers with those skills");
-                        }
-                    })
-                }else{
-                    session.endDialog("Sorry I am not sure I understood.");
-                }
+                sesion.send("Searching for bootcampers");
             }
         }
     ])
