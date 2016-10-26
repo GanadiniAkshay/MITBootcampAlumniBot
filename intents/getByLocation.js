f40919fb9b2e4b0a2678f456dd3fc99d4360925e
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 14:00:43 +0530

    added get by location

--- intents/getByLocation.js
+++ intents/getByLocation.js
@@ -1,12 +1,18 @@
 module.exports = function (intents,builder){
     intents.matches('getByLocation',[
     function (session,args,next){
-        session.send('paris');
+        var User = require('../models/user');
         var location = builder.EntityRecognizer.findEntity(args.entities,'builtin.geography.country');
         if (!location){
             var location = builder.EntityRecognizer.findEntity(args.entities,'builtin.geography.city');
+            if (!location){
+                session.send("Not sure I know what that place is. Try searching by country");
+            }
         }
-        console.log(location);
+        User.find({"residence_country":$in : [location.entity]},function(err, campers){
+            console.log(campers);
+            session.send(found);
+        })
     }
  ]);
 }
