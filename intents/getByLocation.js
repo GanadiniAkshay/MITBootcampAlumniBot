3a469e25f34052870553603ba8f70f2a3488ac15
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 13:50:56 +0530

    added get by location

--- intents/getByLocation.js
+++ intents/getByLocation.js
@@ -2,7 +2,7 @@ module.exports = function (intents,builder){
     intents.matches('getByLocation',[
     function (session,args,next){
         session.send('paris');
-        console.log(args);
+        console.log(builder.EntityRecognizer.findEntity(args.entities,'builtin.geography.country'));
     }
  ]);
 }
