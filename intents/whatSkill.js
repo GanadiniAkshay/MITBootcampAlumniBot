bca34ad66a947a7a2a0c2f6b436a20985a8695ae
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 18:32:43 +0530

    added whatSkill

--- intents/whatSkill.js
+++ intents/whatSkill.js
@@ -5,8 +5,33 @@ module.exports = function (intents,builder){
                 session.endDialog('You can ask general questions about the bootcamp');
             }else{
                 session.send("Searching for bootcampers");
+                var User = require('../models/user');
                 var skill = builder.EntityRecognizer.findEntity(args.entities,'skill');
-                console.log(skill);
+                if (skill){
+                    profession_map = {
+                        "programmer" : 'programming',
+                        "programs"   : 'programming',
+                        "developer"  : 'programming',
+                        "develops"   : 'programming',
+                        "code"       : 'programming',
+                        "coding"     : 'programming',
+                        "coder"      : 'programming',
+                        "codes"      : 'programming',
+                        "paints"     : 'painting',
+                        "painter"    : 'painting',
+                        "photographer" : 'photography',
+                        "videographer" : 'videography',
+                        "seller"       : 'selling',
+                        "teacher"      : 'teaching'
+                    };
+
+                    if (skill in profession_map){
+                        skill = profession_map[skill]
+                    }
+                    session.endDialog(skill);
+                }else{
+                    session.endDialog("Sorry couldn't understand that");
+                }
             }
         }
     ])
