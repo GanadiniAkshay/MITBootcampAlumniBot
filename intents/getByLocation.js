88b70aee95eaebae2b61d3660f05f307484a1822
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 14:04:21 +0530

    added get by location

--- intents/getByLocation.js
+++ intents/getByLocation.js
@@ -11,7 +11,7 @@ module.exports = function (intents,builder){
         }
         User.find({"residence_country":{$in : [location.entity]}},function(err, campers){
             console.log(campers);
-            session.send(found);
+            session.send('found');
         })
     }
  ]);
