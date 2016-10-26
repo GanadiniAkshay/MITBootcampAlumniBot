687681ec2833891b3a00c4fb5eafb33f3e4f04b6
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 14:51:59 +0530

    added get by location in routes

--- routes/getByLocation.js
+++ routes/getByLocation.js
@@ -8,8 +8,23 @@ module.exports = function (bot, builder, User){
             session.sendTyping();
             locations = results.response.split(' ');
             location = locations[locations.length-1].toLowerCase();
-            session.send(location);
-            session.endDialog();
+            User.find({"residence_country":location},function(err,campers){
+            if (campers.length > 0){
+                attachments = [];
+                for (j=0;j<campers.length;j++){
+                    attachments.push(
+                        new builder.HeroCard(session)
+                            .title(campers[j]['name'])
+                            .subtitle(campers[j]["cohort"] + " Lives in " + campers[j]['residence_country'] + '. email : ' + campers[j]['email'])
+                    )
+                }
+                var msg = new builder.Message(session).attachments(attachments);
+                session.endDialog(msg);
+            }
+            else{
+                session.endDialog("Sorry couldn't find anyone");
+            }
+        });
         }
     ]);
 }
