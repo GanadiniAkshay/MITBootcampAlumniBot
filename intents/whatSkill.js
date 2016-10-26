d6239de08ddbd762868462c6489c318b455c5658
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-10-26 15:52:52 +0530

    added whatSkill

--- intents/whatSkill.js
+++ intents/whatSkill.js
@@ -4,7 +4,7 @@ module.exports = function (intents,builder){
             if (session.userData.isBootcamper != 'true'){
                 session.endDialog('You can ask general questions about the bootcamp');
             }else{
-                sesion.send("Searching for bootcampers");
+                session.send("Searching for bootcampers");
             }
         }
     ])
